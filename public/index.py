0e953e47f77c163cccbd30c7f34358ce1afe90da
Max <macsimbodnar@gmail.com>
2017-05-23 14:32:09 +0200

    debug

--- public/index.py
+++ public/index.py
@@ -1,4 +1,11 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+# Turn on debug mode.
+import cgitb
+cgitb.enable()
+
+# Print necessary headers.
+print("Content-Type: text/html")
+print()
 
 print("ciao")
